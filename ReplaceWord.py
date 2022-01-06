b039f0458d8803f8e21ff583224f24c8fb60120c
Davis Burrill <97137252+dabu3393@users.noreply.github.com>
2022-01-05 18:21:10 -0700

    Update ReplaceWord.py
    Deleted some comments

--- ReplaceWord.py
+++ ReplaceWord.py
@@ -14,33 +14,3 @@ with open(input("New File Name: "), 'x') as file:
     file.write(data)
 
 print("Text Replaced " + str(word_count) + " times")
-
-
-
-
-# find_the = stuff.find("the", 0, len(stuff))
-#
-# with open(string) as openfileobject:
-#     for line in openfileobject:
-#         find_the = stuff.find("the", 0, len(line))
-#         the_count += 1
-#         while
-#         find_the = stuff.find("the", find_the + 1, len(line))
-#         the_count += 1
-#
-# print(len(stuff))
-#
-# print(find_the)
-
-# for i in stuff:
-#     if i.isspace():
-#         space_count += 1
-#     elif i.isupper():
-#         upper_count += 1
-#     else:
-#         lower_count += 1
-#
-#
-# print("Space Count: " + str(space_count))
-# print("Uppercase Count: " + str(upper_count))
-# print("Lowercase Count: " + str(lower_count))
